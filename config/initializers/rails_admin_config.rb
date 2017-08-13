a26c88a209f44c1cfb092f42bc076cde91afde77
fatloris <kashmatov@mail.ru>
2017-08-13 19:01:39 +0300

    administrative functinality added

---
+++ config/initializers/rails_admin_config.rb
@@ -0,0 +1,23 @@
+# RailsAdmin.config do |config|
+#   config.main_app_name = ["Busfor Test App", "Админ"]
+
+#   config.actions do
+#     dashboard
+#     index
+#     new
+#     bulk_delete
+#     show
+#     edit
+#     delete
+#   end
+
+#   Rails.application.eager_load!
+
+#   ApplicationRecord.descendants.each do |model_class|
+#     model = model_class.to_s.underscore
+#     config.model model.capitalize do
+#       label I18n.t("admin.#{model}.singular")
+#       label_plural I18n.t("admin.#{model}.plural")
+#     end
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: "routes#search"
   get 'index', to: 'routes#search'
   get 'index', to: 'routes#search'
