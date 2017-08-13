01c2394dcc6f318940930f4b88096f3054bbcd90
fatloris <kashmatov@mail.ru>
2017-08-13 19:11:10 +0300

    little tune

--- app/models/route_log.rb
+++ app/models/route_log.rb
@@ -1,4 +1,8 @@
 class RouteLog < ApplicationRecord
+  validates_presence_of :start_station,
+                        :finish_station,
+                        :carrier,
+                        :currency
   belongs_to :start_station, class_name: 'Station'
   belongs_to :finish_station, class_name: 'Station'
   belongs_to :carrier
